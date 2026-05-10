65a9ea81f7fb99b67ffbb128906ce6be0da080b5
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-10 13:42:01 -0300

    comentarios

---
+++ src/introducao/comentario.java
@@ -0,0 +1,17 @@
+package introducao;
+
+public class comentario {
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(
+        "comentario ou nao ?!" +
+        "comentarios  // em linha " + // em linha
+        "/* " +
+        " /** doc ");
+
+    }
+}
