5e702f18977c4f6f98710ee1ba6eb88be10fd3cd
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-10 01:06:29 -0300

    primeira atividade

--- src/maratona_java_Dev_Dojo/Execicio_01.java
+++ src/maratona_java_Dev_Dojo/Execicio_01.java
@@ -1,4 +1,17 @@
 package maratona_java_Dev_Dojo;
 
-public class Execicio_01 {
+public class Execicio_01
+{
+
+
+    public static void main(String[] args)
+    {
+
+
+        System.out.println("Olá, DevDojo! Começando o exercício 01");
+
+
+
+
+    }
 }
