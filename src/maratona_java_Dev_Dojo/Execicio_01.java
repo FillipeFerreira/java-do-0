fc5a925749b7f56f66f9006d7d69718ece11cac4
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-12 15:06:59 -0300

    usando try e catch tratamento de erro

--- src/maratona_java_Dev_Dojo/Execicio_01.java
+++ src/maratona_java_Dev_Dojo/Execicio_01.java
@@ -11,34 +11,45 @@ public class Execicio_01 {
 
 
         LocalDate hoje = LocalDate.now();
-
-
-        String mesPt = hoje.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt", "BR"));
+        Locale brasil = new Locale("pt", "BR");
+        String mesPt = hoje.getMonth().getDisplayName(TextStyle.FULL, brasil);
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Qual seu nome?");
-        String nome = sc.nextLine();
 
-        System.out.println("Qual endereco voce mora?");
-        String endereco = sc.nextLine();
+        try {
+            System.out.println("Qual seu nome?");
+            String nome = sc.nextLine();
 
-        System.out.println("Quanto de salario voce recebe?");
-        float salario = sc.nextFloat();
+            System.out.println("Qual endereco voce mora?");
+            String endereco = sc.nextLine();
 
-        System.out.println("Qual o dia do pagamento (1 a 31)?");
+            System.out.println("Quanto de salario voce recebe?");
+            float salario = sc.nextFloat();
 
-        byte diaPagamento = sc.nextByte();
+            System.out.println("Qual o dia do pagamento (1 a 31)?");
+            byte diaPagamento = sc.nextByte();
 
-        if ((diaPagamento > 0) && (diaPagamento < 31))
-        System.out.println("Eu " + nome + ", morando no " + endereco +
-                ", confirmo que recebi o salario de R$ " + salario +
-                " no dia " + diaPagamento + " do mes de " + mesPt);
-        else {
-            System.out.println("numero invalido, escreva numero de 1 a 31");
+
+            if (diaPagamento >= 1 && diaPagamento <= 31) {
+                System.out.println("\n--- COMPROVANTE ---");
+                System.out.println("Eu " + nome + ", morando no " + endereco +
+                        ", confirmo que recebi o salario de R$ " + salario +
+                        " no dia " + diaPagamento + " do mes de " + mesPt);
+            } else {
+                System.out.println("Erro: O dia digitado é inválido (deve ser entre 1 e 31).");
+            }
+
         }
 
+        catch (Exception e) {
+            System.out.println("Erro: Entrada inválida! Por favor, use apenas números para salário e data.");
+            System.out.println("Tipo do erro: " + e.toString());
+        }
 
-        sc.close();
+        finally {
+            sc.close();
+            System.out.println("\nSistema encerrado.");
+        }
     }
 }
