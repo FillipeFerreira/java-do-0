1cbc4131c38b248426d1ef9a0598aedb66425193
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-12 12:45:36 -0300

    tipos primitivos

---
+++ src/introducao/tiposPrimitivos.java
@@ -0,0 +1,32 @@
+package introducao;
+
+public class tiposPrimitivos {
+    public static void main(String[] args)
+
+    {// Tipos primitivos : int,double, float, char,short, long , boolean.
+        int idade = 10;
+        double salarioDOuble = 2000;
+        long numerosGrandes = 100000;
+        float salarioFloat = 2500;
+        byte idadeByte= 10;
+        short idadeShort =10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere ='M';
+        char caracter2 = '\u0041';
+
+        /*---------------------------------------------------------------------------------------------------*/
+        System.out.println(idade);
+        System.out.println(false);
+        System.out.println(salarioFloat);
+        System.out.println(salarioDOuble);
+        System.out.println( numerosGrandes);
+        System.out.println(idadeByte);
+        System.out.println(idadeShort);
+        System.out.println(verdadeiro);
+        System.out.println(falso);
+        System.out.println(caractere);
+        System.out.println(caracter2+" comentario ");
+
+    }
+}
