c7e7d6d6f0b3ddd496a57f83ec9c6a77109cf0bc
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-12 13:43:49 -0300

    String & e var

--- src/introducao/tiposPrimitivos.java
+++ src/introducao/tiposPrimitivos.java
@@ -5,16 +5,16 @@ public class tiposPrimitivos {
 
     {// Tipos primitivos : int,double, float, char,short, long , boolean.
         int idade = 10;
-        double salarioDOuble = 2000;
+        double salarioDOuble = 2000.0;
         long numerosGrandes = 100000;
-        float salarioFloat = 2500;
+        float salarioFloat = 20.0F;
         byte idadeByte= 10;
         short idadeShort =10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere ='M';
         char caracter2 = '\u0041';
-
+        var numeros = 12;
         /*---------------------------------------------------------------------------------------------------*/
         System.out.println(idade);
         System.out.println(false);
@@ -26,7 +26,15 @@ public class tiposPrimitivos {
         System.out.println(verdadeiro);
         System.out.println(falso);
         System.out.println(caractere);
-        System.out.println(caracter2+" comentario ");
+        System.out.println(caracter2+"Comment");
+        System.out.println(numeros);
+
+
+       /*-----------------------------------------------------------------------------------*/
+
+
+        String falandoString = "It is not a primitive type";
+        System.out.println(falandoString);
 
     }
 }
