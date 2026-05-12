540e31784b04d6770a275f3124ae2d2e621e5681
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-12 15:48:26 -0300

    Tipos de atribuicao e comparacao > < == !=

---
+++ src/maratona_java_Dev_Dojo/Execicio_03_operadores.java
@@ -0,0 +1,40 @@
+package maratona_java_Dev_Dojo;
+
+public class Execicio_03_operadores {
+
+    public static void main(String[] args)
+    {
+        //operadores  + - / * //numeros inteiros.
+
+        int numeros  = 10;
+        int numero3  = 20;
+
+        System.out.println( numero3 * numeros);
+        System.out.println( numero3 / numeros);
+        System.out.println( numero3 + numeros);
+        System.out.println( numero3 - numeros);
+
+        System.out.println("---------------------------------------------------------------------------");
+
+        int resto = 21 % 2;
+
+        System.out.println(resto);
+
+        System.out.println("----------------------------------------------------------------------------");
+
+        //  < >  <= >=  atribuicao    comparacao == diferente !
+
+        boolean isDezMaiorQueVinte = 10 > 20 ;
+        boolean isDezIgualAvinte = 10 == 20 ;
+        boolean isDezMenorQueVinte = 10 > 20 ;
+        boolean isDezDiferenteVinte = 10 != 20;
+
+        System.out.println("isDezMaiorQueVinte ="+isDezMaiorQueVinte);
+        System.out.println("isDezIgualAvinte ="+isDezIgualAvinte);
+        System.out.println("isDezMenorQueVinte ="+isDezMenorQueVinte);
+        System.out.println("isDezDiferenteVinte = "+isDezDiferenteVinte);
+
+
+
+    }
+}
