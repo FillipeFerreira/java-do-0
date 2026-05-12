dc74325276e2b70178fba47739f6e3301a95079d
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-12 15:04:23 -0300

    exercicio 1 usando variaveis

--- src/maratona_java_Dev_Dojo/Execicio_01.java
+++ src/maratona_java_Dev_Dojo/Execicio_01.java
@@ -1,16 +1,44 @@
 package maratona_java_Dev_Dojo;
 
-public class Execicio_01
-{
+import java.time.format.TextStyle;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class Execicio_01 {
+
+    public static void main(String[] args) {
 
 
-    public static void main(String[] args)
-    {
+        LocalDate hoje = LocalDate.now();
 
 
-        System.out.println("Olá, DevDojo! Começando o exercício 01");
+        String mesPt = hoje.getMonth().getDisplayName(TextStyle.FULL, new Locale("pt", "BR"));
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual seu nome?");
+        String nome = sc.nextLine();
+
+        System.out.println("Qual endereco voce mora?");
+        String endereco = sc.nextLine();
+
+        System.out.println("Quanto de salario voce recebe?");
+        float salario = sc.nextFloat();
+
+        System.out.println("Qual o dia do pagamento (1 a 31)?");
+
+        byte diaPagamento = sc.nextByte();
+
+        if ((diaPagamento > 0) && (diaPagamento < 31))
+        System.out.println("Eu " + nome + ", morando no " + endereco +
+                ", confirmo que recebi o salario de R$ " + salario +
+                " no dia " + diaPagamento + " do mes de " + mesPt);
+        else {
+            System.out.println("numero invalido, escreva numero de 1 a 31");
+        }
 
 
-
+        sc.close();
     }
 }
