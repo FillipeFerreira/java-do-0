c5cac1a0f02162b12027d69aa3d21071601eafe4
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-12 15:29:35 -0300

    operadores + - / *, numeros inteiros

---
+++ src/maratona_java_Dev_Dojo/Execicio_02_operadores.java
@@ -0,0 +1,25 @@
+package maratona_java_Dev_Dojo;
+
+public class Execicio_02_operadores {
+
+    public static void main(String[] args)
+    {
+        //operadores  + - / * //numeros inteiros.
+
+        int numeros  = 10;
+        int numero3  = 20;
+
+
+
+        System.out.println( numero3 * numeros);
+        System.out.println( numero3 / numeros);
+        System.out.println( numero3 + numeros);
+        System.out.println( numero3 - numeros);
+
+
+
+
+
+
+    }
+}
