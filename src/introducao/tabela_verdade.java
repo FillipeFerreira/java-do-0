bab2402d857a9bee4243315ca984d2e1272b3a92
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-16 10:42:00 -0300

    tabela  verdade

---
+++ src/introducao/tabela_verdade.java
@@ -0,0 +1,23 @@
+package introducao;
+
+public class tabela_verdade {
+    public static void main(String[] args) {
+
+
+        //              V == Verdadeiro F == Falso " && "  || "
+        //           V && v = v
+        //           v && F = F
+        //           v && F = F
+        //           F && F = F
+        //
+        //           V || F = V
+        //           F || V = V
+        //           V || V = V
+        //           F || F = F
+        //
+        // */
+
+
+
+    }
+}
