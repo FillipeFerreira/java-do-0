44346fffa0b11e70a40d7aadb42359cc2d05c8ea
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-16 10:33:57 -0300

    if else simples, como reduzir

---
+++ src/maratona_java_Dev_Dojo/xercicio_09_EstruturasCondicionais.java
@@ -0,0 +1,26 @@
+package maratona_java_Dev_Dojo;
+import  java.util.Scanner;
+
+
+
+public class xercicio_09_EstruturasCondicionais {
+    public static void main(String[] args)
+
+    {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Qual e seu salario ? vamos ver se vai doar para o devDojo");
+
+        double salario = scanner.nextDouble();
+        String doarDevDojo = "vai doar 500,00 pro devDojo";
+        String naoVaiDoarDevDojo = "nao vai doar 500 pro  devDojo";
+
+        String resultado = salario >=5000.01 ? doarDevDojo :naoVaiDoarDevDojo;
+
+
+        System.out.println(resultado);
+
+
+
+
+    }
+}
