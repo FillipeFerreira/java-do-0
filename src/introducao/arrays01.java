72d9ec0ed59c67d669a9b217a5d0e7235faec9a7
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-18 11:28:01 -0300

    arrys com while

--- src/introducao/arrays01.java
+++ src/introducao/arrays01.java
@@ -3,15 +3,18 @@ package introducao;
 public class arrays01 {
     public static void main(String[] args)
 
+    {
 
-    {  int [] idades = new int[3];
-        idades [0]=1;
-        idades [1]=2;
-        idades [2]=3;
-        System.out.println(idades[0]);
-        System.out.println(idades[1]);
-        System.out.println(idades[2]);
+        int [] idades = new int[] {2,2,3};
 
+
+        idades [0] = 0;
+        idades [1] = 1;
+        idades [2] = 2;
+
+        while ( idades [2] > idades [0]){
+            System.out.println("e maior");
+            idades [0] +=1; }
 
 
 
