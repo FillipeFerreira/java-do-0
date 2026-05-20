5f5270fde7f87a0b49316972d4099dcdb8c62950
Fillipe <fillipeferreiranm@hotmail.com>
2026-05-20 00:53:22 -0300

    arrys revisao

--- src/introducao/arrays03.java
+++ src/introducao/arrays03.java
@@ -5,34 +5,40 @@ public class arrays03 {
     public static void main(String[] args) {
 
 
-        int[][] nomes = new int[3][2];
+        int[][] nomes = new int[3][3];
 
         nomes[0][0] = 0;
         nomes[0][1] = 1;
+        nomes[0][2] = 1;
 
         nomes[1][0] = 2;
         nomes[1][1] = 3;
+        nomes[1][2] = 3;
 
         nomes[2][0] = 4;
         nomes[2][1] = 5;
-
-        System.out.println(nomes[0][0]);
-        System.out.println(nomes[0][1]);
-        System.out.println(nomes[1][0]);
-        System.out.println(nomes[1][1]);
-        System.out.println(nomes[2][0]);
-        System.out.println(nomes[2][1]);
+        nomes[2][2] = 5;
 
 
-        for (int[] linha : nomes) {
-            for (int numero : linha) {
-                System.out.println(numero);
+        for (int i = 0; i < nomes.length; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.println(nomes[i][j]);
+            }
+
+
+
+        }
 
             }
 
         }
 
 
-    }
 
-}
+
+
+
+
+
+
+
